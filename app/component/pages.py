64ae2b56d64c9b468aa4110aa64a4ec34b9e372e
ohmmaagoch <ohm@42bangkok.com>
2024-05-30 15:52:30 +0700

    feat: Update page link to Ticker.py in Pages class

--- app/component/pages.py
+++ app/component/pages.py
@@ -5,6 +5,6 @@ class Pages:
     ...
 
   def page(self):
-    st.page_link("pages/Stock.py", label="Stock", icon="1️⃣")
+    st.page_link("pages/Ticker.py", label="Ticker", icon="1️⃣")
     st.page_link("pages/Counter.py", label="Counter", icon="2️⃣")
     # st.page_link("http://www.google.com", label="Google", icon="🌎")
